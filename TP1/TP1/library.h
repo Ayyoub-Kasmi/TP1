da50e9b6c921d4199edc6250658aa82e6e072a56
grine2003 <loordmohammed344@gmail.com>
2022-03-20 17:36:58 +0100

    Beginning
    First commit

--- TP1/TP1/library.h
+++ TP1/TP1/library.h
@@ -1,25 +1,6 @@
 #ifndef LIBRARY_H_INCLUDED
 #define LIBRARY_H_INCLUDED
 
-//For the first presentation
-typedef struct Node1 {
-    int val;
-    int row;
-    int col;
-    struct Node1* next;
-} Node1;
 
-//For the second presentation
-typedef struct Line{
-    int number; //line number
-    struct Node2* first; //first element in the line
-    struct Line* next; //to move to the next line
-} Line;
-
-typedef struct Node2* {
-    int val;
-    int col;
-    struct Node2* next;
-} Node2;
 
 #endif // LIBRARY_H_INCLUDED
--- TP1/TP1/main.c
+++ TP1/TP1/main.c
@@ -1,16 +1,99 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
 #include "library.h"
+
+ typedef struct Maillon_vecteur{
+
+      int val;
+      int ligne;
+      int colonne;
+      struct Maillon_vecter *next;
+    }Maillon_vecteur;
+
+    //***************************   Menu !  ****************************************
+    // 1)************ Lecture d'une matrice et création de la liste ****************
+    // 2)************ Affichage d’une matrice **************************************
+    // 3)************ Extraction d’une sous matrice*********************************
+    // 4)************ Division d’une matrice en sous-blocs de taille égale**********
+    // 5)************ Addition de deux ou plusieurs matrices.***********************
+    // 6)************ Multiplication par vecteur, multiplication de deux matrices***
+    // 7)************ Transposée d’une matrice**************************************
+    // 8)************Opérations logiques : NON, ET, OU, OU exclusif*****************
+
 
 int main()
 {
-    int m, n; //size of the m*n matrix
-    Node2 head; //in fact we need to choose wether head is of type Node1 or Node2 according to the chosen presentation
-    printf("Enter the size of the matrix (lines, columns): ");
-    scanf("%d %d", &m, &n);
+    // 1) Lecture d'une matrice et création de la liste
+   Maillon_vecteur *p=NULL;
+   p=(Maillon_vecteur *)malloc(sizeof(Maillon_vecteur));
+   Maillon_vecteur *tete;
+   if (p!=NULL){
+    tete=p;
+   }
+   int i,j,li,cl;
+   printf("Donner le nombre de lignes : ");
+   scanf("%d",&li);
+   printf("Donner le nombre de collones : ");
+   scanf("%d",&cl);
+  for (i=1;i<=li;i++){ // begin for i
+     for (j=1;j<=cl;j++){ // begin for j
+      p->ligne=i;
+      p->colonne=j;
+      printf("T[%d,%d]=",p->ligne,p->colonne);
+      scanf("%d",&p->val);
+      if (p->val==0){
+        continue;
+      }
+        p->next=(Maillon_vecteur *)malloc(sizeof(Maillon_vecteur));
+        p=p->next;
+    }//end for j
+   }//end for i
+   p->next=NULL;
 
-    //implementing the second presentation
-    createMatrix(head);
+  /* // 2)Affichage d’une matrice
+   Maillon_vecteur *q=tete;
+  for (i=1;i<=li;i++){ // begin for i
+     for (j=1;j<=cl;j++){// begin for j
+      printf("%d | ",q->val);
+      q=q->next;
+     } // end for j
+     printf("\n");
+   }//end for i*/
 
+   // 3)Extraction d’une sous matrice
+
+   int ip,jp,id,jd;
+   Maillon_vecteur *E=tete;
+   printf("donner le nemuro de la ligne de la premiere case : ");
+   scanf("%d",&ip);
+   printf("donner le nemuro de la colonne de la premiere case : ");
+   scanf("%d",&jp);
+   printf("donner le nemuro de la ligne de la derniere case : ");
+   scanf("%d",&id);
+   printf("donner le nemuro de la colonne de la derniere case : ");
+   scanf("%d",&jd);
+   for (i=1;i<=li;i++){ // for i
+    for (j=1;j<=cl;j++){ // for j
+    if (E->ligne>=ip && E->colonne<=jd)
+    {
+        printf("%d | ",E->val);
+    }
+    if (E->ligne==id && E->colonne==jd)
+    {
+        E->next=NULL;
+        break;
+
+    }
+    E=E->next;
+
+    } // end for j
+    printf("\n");
+    if (E->ligne==id && E->colonne==jd)
+        break;
+   } // end for i
+
+   // 4) Division d’une matrice en sous-blocs de taille égale
     return 0;
 }
